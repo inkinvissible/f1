c60f5e0e5657b25bafa29b03f3346e12d4c26ead
Martin Cappelletti Agustín <93543220+inkinvissible@users.noreply.github.com>
2024-11-20 15:23:41 +0000

    api con fectch

--- api/proxy.js
+++ api/proxy.js
@@ -1,16 +1,16 @@
-const data = null;
-
-const xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
-
-xhr.addEventListener('readystatechange', function () {
-	if (this.readyState === this.DONE) {
-		console.log(this.responseText);
+const url = 'https://f1-motorsport-data.p.rapidapi.com/schedule?year=2024';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '4af36d482fmshbeb403eefe984abp11731ejsnade543db6b29',
+		'x-rapidapi-host': 'f1-motorsport-data.p.rapidapi.com'
 	}
-});
+};
 
-xhr.open('GET', 'https://f1-motorsport-data.p.rapidapi.com/schedule?year=2024');
-xhr.setRequestHeader('x-rapidapi-key', '4af36d482fmshbeb403eefe984abp11731ejsnade543db6b29');
-xhr.setRequestHeader('x-rapidapi-host', 'f1-motorsport-data.p.rapidapi.com');
-
-xhr.send(data);
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
