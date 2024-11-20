26e27eba16ef316bd01b011ae922d097bea3e744
Martin Cappelletti Agustín <93543220+inkinvissible@users.noreply.github.com>
2024-11-20 15:42:39 +0000

    Headers

--- api/proxy.js
+++ api/proxy.js
@@ -1,4 +1,14 @@
 export default async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Permite solicitudes desde cualquier origen
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        // Manejo de preflight
+        res.status(200).end();
+        return;
+    }
+
     const url = 'https://f1-motorsport-data.p.rapidapi.com/schedule?year=2024';
     const options = {
         method: 'GET',
