7756734f99016897fb3662713c2a04b5e483ace6
Martin Cappelletti Agustín <93543220+inkinvissible@users.noreply.github.com>
2024-11-20 22:25:44 +0000

    2025 año

--- api/proxy.js
+++ api/proxy.js
@@ -5,8 +5,8 @@ export default async function handler(req, res) {
 
 	const baseUrl = 'https://f1-motorsport-data.p.rapidapi.com';
     const endpoints = [
-        '/standings-controllers?year=2024',
-        '/standings-drivers?year=2024',
+        '/standings-controllers?year=2025',
+        '/standings-drivers?year=2025',
     ];
 
     const options = {
